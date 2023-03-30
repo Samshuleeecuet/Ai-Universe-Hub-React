38e1ffb421617a23fc28cbb9c9dcf3f05287d08b
Samshul Islam Shakil <samshuleeeweb@gmail.com>
2023-03-30 20:53:22 +0600

    Complete

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const Modal = (props) => {
-    const {description,features,image_link,integrations} = props.singleData;
-    console.log(Object.values(features || {}));
+    const {description,features,image_link,integrations
+    } = props.singleData;
+    console.log(integrations
+        );
     return (
         <>
         <input type="checkbox" id="my-modal-5" className="modal-toggle" />
@@ -13,10 +15,15 @@ const Modal = (props) => {
                     <h2 className="card-title">{description? description :"Not Found"}</h2>
                     <div className="flex justify-between">
                         <div className="features">
-                            <h1 className='text-3xl font-bold'>Features</h1>
+                            <h1 className='text-3xl font-bold pb-4'>Features</h1>
+                            {
+                                Object.values(features || {}).map((value ,index)=>(<p className='text-left'>{index+1} {value.feature_name? value.feature_name:"Not Found"}</p>))
+                            }
                         </div>
-                        <div className="intregation text-3xl font-bold">
-                            <h1 className='text-3xl font-bold'>Intregation</h1>
+                        <div className="intregation">
+                            <h1 className='text-3xl font-bold pb-4'>Intregation</h1>
+                            {
+                               integrations?integrations.map((value ,index)=>(<p className='text-left'>{index+1} {value}</p>)):"Not Found"}
 
                         </div>
                     </div>
